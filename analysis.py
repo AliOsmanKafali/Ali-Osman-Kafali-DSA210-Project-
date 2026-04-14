d9dead29440a61c9dea623b86669f39e37d8a618
AliOsmanKafali <ali.kafali@sabanciuniv.edu>
2026-04-14 19:04:40 +0300

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -2,3 +2,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 print("DSA210 milestone 1 started")
+
+# Dataset will be loaded here
+# df = pd.read_csv("spotify.csv")
+
+print("Initial data analysis started")
+
+# Example operations (to be completed later)
+# print(df.head())
+# print(df.describe())
+
+# Example visualization (placeholder)
+# df['popularity'].hist()
+# plt.title("Popularity Distribution")
+# plt.show()
